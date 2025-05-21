8257ee44719ba0bfa60baac6ba59ce70e07e348e
drewsephski <drewsepeczi@gmail.com>
2025-05-21 17:52:12 -0500

    fix: update ref type to handle null values

--- src/components/hooks/use-elastic-line-events.ts
+++ src/components/hooks/use-elastic-line-events.ts
@@ -8,7 +8,7 @@ interface ElasticLineEvents {
 }
 
 export function useElasticLineEvents(
-  containerRef: React.RefObject<SVGSVGElement>,
+  containerRef: React.RefObject<SVGSVGElement | null>,
   isVertical: boolean,
   grabThreshold: number,
   releaseThreshold: number,
